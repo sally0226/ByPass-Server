7b7cd231fb567941d8410638a39370eb546bcf49
daisy-kim <sally0226@naver.com>
2024-07-02 02:15:40 +0900

    Feat: project setting
    - API docs 세팅 (/api-docs)
    - auth, uses 도메인 생성

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('ByPass API')
+    .setDescription('스쳐가는머니(ByPass) API Docs')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
